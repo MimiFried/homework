cd109f7ebe332c9a231ff86b0fe91a6b8d18e4f9
MimiFried <nechamamfried@gmail.com>
2020-11-22 21:53:54 -0500

    homework 78

---
+++ Javascript/78/hw78.js
@@ -0,0 +1,23 @@
+(function (){
+'use strict';
+
+const numInput = document.getElementById('theNum');
+const resultElem = $('#result');
+
+const number = Math.floor(Math.random() * 10) + 1;
+console.log(number);
+numInput.addEventListener('change', () => {
+    let guess = Number($('#theNum').val());
+    let result;
+    if(guess === number){
+        result = 'You guessed the right number!!';
+    }
+    else if(guess > number){
+        result = 'You guessed too high';
+    }
+    else{
+        result = 'You guessed too low';
+    }
+    $(`<div>${guess}: ${result}</div>`).appendTo(resultElem);
+  });
+})();
