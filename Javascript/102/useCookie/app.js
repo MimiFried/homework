41c91d8b99d883032d29613e9acf1d8912dede0e
MimiFried <nechamamfried@gmail.com>
2021-04-26 22:00:48 -0400

    homework 102

---
+++ Javascript/102/useCookie/app.js
@@ -0,0 +1,56 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/pageVisited', (req, res, next)=>{
+  const count = (+req.cookies.count || 0) +1;
+  res.cookie('count', count);
+  res.send(`You have visited this site ${count} amount of times.`);
+});
+
+app.get('/', (req, res, next)=>{
+  const name = req.query.name || req.cookies.name || '';
+  
+  res.cookie('name', name);
+  
+  res.locals.name = name;
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
