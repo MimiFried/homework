f52aafa259698ee4fdfeba86f10efc76fcfc7705
MimiFried <nechamamfried@gmail.com>
2021-06-13 18:00:36 -0400

    homework 109

---
+++ Javascript/109/app.py
@@ -0,0 +1,9 @@
+my_name = 'nechama m fried'
+my_address = '29 Kedma Drive'
+sibs = ['yona', 'pessie', 'shloime', 'yaakov', 'Breindel', 'Michoel', 'Moishe']
+print(f'Name: {my_name} \nAddress: {my_address}\nSiblings:{sibs}')
+
+print(my_name[::3])
+
+michoel = sibs[-2]
+print(michoel[1:-1])
